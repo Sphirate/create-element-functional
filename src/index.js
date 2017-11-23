f406ea3f66b3e0cefce4769082e4ec6b8f71029d
Dmitry Rykov <sphirate@gmail.com>
2017-11-23 16:02:45 +0300

    Fix single child is wrapped in array

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 
+const isPureSFComponent = (component) => (
+    typeof component === 'function' &&
+    component.prototype instanceof React.Component === false &&
+    !component.contextTypes
+);
+
+const formatChildren = (children) => children.length > 1 ? children : children[0];
+
 export default (component, props, ...children) => {
-    if (
-        typeof component === 'function' &&
-        component.prototype instanceof React.Component === false &&
-        !component.contextTypes
-    ) {
-        return component(Object.assign({}, component.defaultProps, { ...props, children }), {});
+    if (isPureSFComponent(component)) {
+        return component({ ...component.defaultProps, ...props, children: formatChildren(children) }, {});
     }
 
     return React.createElement(component, props, ...children);
